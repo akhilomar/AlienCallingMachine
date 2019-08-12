04854613db185c771be9d7840fbf1c710ccaea9e
akhilomar <akhilomar555@gmail.com>
2019-08-12 19:46:42 +0530

    Alien Calling Machine

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import DrumPad from './drumpad';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  { id: 'Ghuurqol', letter: 'Q', src: 'https://www.myinstants.com/media/sounds/q_KWyYNtU.mp3' },
+  { id: 'Uczek', letter: 'W', src: 'https://www.myinstants.com/media/sounds/w_4WPlOQ5.mp3' },
+  { id: 'Cair\'ods', letter: 'E', src: 'https://www.myinstants.com/media/sounds/c_jNuXsFa.mp3' },
+  { id: 'Thrauns', letter: 'A', src: 'https://www.myinstants.com/media/sounds/a_wqQWATd.mp3' },
+  { id: 'Aelan', letter: 'S', src: 'https://www.myinstants.com/media/sounds/e_rhlMBwk.mp3' },
+  { id: 'Manger', letter: 'D', src: 'https://www.myinstants.com/media/sounds/f_kMG591C.mp3' },
+  { id: 'Visol', letter: 'Z', src: 'https://www.myinstants.com/media/sounds/z_VFf6aN8.mp3' },
+  { id: 'Kolphins', letter: 'X', src: 'https://www.myinstants.com/media/sounds/x_J6FWq5F.mp3' },
+  { id: 'Ghamins', letter: 'C', src: 'https://www.myinstants.com/media/sounds/c_Xi6274T.mp3'  },
+]
+
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      display:'Click Key To Play'
+    }
+  }
+
+handleDisplay=display=>this.setState({display});
+
+  render(){
+    return(
+      <div id="drum-machine"> 
+        <div id="display">{this.state.display}</div>
+        <div id="drum-pads">
+        {data.map(d=>(
+          <DrumPad id={d.id} letter={d.letter} src={d.src} handleDisplay={this.handleDisplay}
+           />
+        ))}
+        </div>
+       <p><strong>KOI MIL GAYA (Alien Calling Key Note) : </strong>WQDZWQSAWQDZAEQ</p>
+      </div>
+
+    )
+  }
+
 }
 
 export default App;
---
+++ src/drumpad.js
@@ -0,0 +1,50 @@
+import React,{Component} from 'react';
+import './App.css';
+
+
+
+class DrumPad extends Component{
+    constructor(props){
+        super(props);
+
+        this.handleClick=this.handleClick.bind(this);
+    }
+    componentDidMount(){
+        document.addEventListener('keydown',this.handleKeyDown)
+        window.focus()
+    }
+    componentWillUnmount(){
+            document.removeEventListener('keydown',this.handleKeyDown)
+    }
+
+    handleKeyDown = e => {
+        if(e.keyCode=== this.props.letter.charCodeAt()){
+            this.audio.play()
+            this.audio.currentTime=0
+            this.props.handleDisplay(this.props.id)
+        }
+    }
+    handleClick = () => {
+        this.audio.play()
+        this.audio.currentTime = 0
+        this.props.handleDisplay(this.props.id)
+      }
+      
+
+    render(){
+        return(
+            <div className="drum-pad" id={this.props.id} onClick={this.handleClick}>
+            <h1>{this.props.letter}</h1>
+            <audio
+            ref={ref => this.audio=ref}
+                className="clip"
+                src={this.props.src}
+                id={this.props.letter}>
+            </audio>
+
+            </div>    
+        );
+    }
+}
+
+export default DrumPad;
